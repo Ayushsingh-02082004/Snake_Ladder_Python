8abe83fde9fe88d4be49f33502c27c304868b7bf
Ayush singh <ayushsingh02082004@gmail.com>
2026-03-13 00:22:47 +0530

    final commit uc 7

--- Snake and Ladder/main.py
+++ Snake and Ladder/main.py
@@ -6,38 +6,51 @@ def main():
 
     print("-----Welcome to the Snake and Ladder Game------")
 
-    #uc1
-    player1 = Player()
+    #uc7
+
+    players = [Player() , Player()]
+    player_names = ["Player 1" , "Player 2"]
 
     options = ("NoPlay" , "Ladder" , "Snake")
-    WINNING_POSITION = 100
+
+    WINNING_POSSITION = 100
     roll_count = 0
+    current_player_idx = 0
+    game_won = False
 
-    #uc4 Repeat until the player reachess the winning position 
-    while player1.position < WINNING_POSITION:
+    while not game_won:
+
         roll_count += 1
+        current_player = players[current_player_idx]
+        current_name = player_names[current_player_idx]
 
-        #Uc2 roll the die
         roll = Dice.roll()
-
         action = choice(options)
 
-        match action :
+        match action:
             case "NoPlay":
                 pass
-            case "Ladder"  if player1.position +roll <= WINNING_POSITION :
-                player1.position += roll
-                print(f"Roll {roll_count} : Die({roll}) | Action: {action:7} | Position : {player1.position}")
+            case "Ladder" if current_player.position + roll <= WINNING_POSSITION:
+                current_player.position += roll
+                print(f"Roll {roll_count:3} | {current_name} | Die: {roll} | Action: {action:7} | Position: {current_player.position}")
             case "Ladder":
                 pass
-                print(f"Roll {roll_count} : Die({roll}) | Action: {action:7} | Position : {player1.position}")
+                print(f"Roll {roll_count:3} | {current_name} | Die: {roll} | Action: {action:7} | Position: {current_player.position}")
             case "Snake":
-                player1.position = max(0 , player1.position-roll)
-                print(f"Roll {roll_count} : Die({roll}) | Action: {action:7} | Position : {player1.position}")
+                current_player.position = max(0, current_player.position - roll)
+                print(f"Roll {roll_count:3} | {current_name} | Die: {roll} | Action: {action:7} | Position: {current_player.position}")
+
+        #check for winner
+        if current_player.position == WINNING_POSSITION:
+            print("-" * 45)
+            print(f"Congratulations  {current_name} WON THE GAME in {roll_count} total rolls! ")
+            game_won  = True
+        else:
+            #switch to the other player (0 becomes 1 , 1 becomes 0)
+            current_player_idx = 1-current_player_idx
 
 
-    print("-" * 40)
-    print(f"WINNER! You reached {player1.position} in {roll_count} rolls.")
+
 
 if __name__ == "__main__":    
     main()
